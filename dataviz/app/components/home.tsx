3b6481099769ef6e2261bcc774b7b96520fbb6d9
Mayankpratapsingh022 <mayankpratapsingh022@gmail.com>
2024-12-26 15:06:04 +0530

    Home page coded

---
+++ dataviz/app/components/home.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+const Topics = () => {
+  const topics = [
+    { name: "Linear Regression", path: "/linear-regression" },
+    { name: "Logistic Regression", path: "/logistic-regression" },
+    { name: "Support Vector Machines", path: "/svm" },
+    { name: "Decision Trees", path: "/decision-trees" },
+  ];
+
+  return (
+    <div className="bg-gray-950 h-screen flex flex-col  items-center justify-center w-full  text-white p-6">
+    <section className="text">
+    <h1 className="text-lg font-medium flex text-left">Topics</h1>
+
+    </section>
+      <ul className="flex justify-center items-start flex-col gap-4">
+        {topics.map((topic, index) => (
+          <li key={index} className="text-gray-400 hover:text-white transition-colors duration-200">
+            <Link
+              href={topic.path}
+              className="text-gray-100 underline hover:text-gray-300"
+            >
+              {topic.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Topics;
---
+++ dataviz/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import Topics from "./components/home";
+export default function Home() {
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+     <Topics/>
+    </div>
+  );
+}
